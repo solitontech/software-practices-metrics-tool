bae081db970e711bef6ae43bd8fd1c05463911ab
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-02-26 15:08:08 +0530

    BE | Add integration test for client filters. (#13)
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

---
+++ Server/tests/integration/client-filters/client-filters.test.js
@@ -0,0 +1,14 @@
+import request from 'supertest';
+import { describe, it, expect } from '@jest/globals';
+
+import { ServerConfiguration } from '../../../src/configs/server-config.js';
+import app from '../../../src/index.js';
+
+describe('Client-filters - get client filters configured in server-config.json', () => {
+  it('should return client filters(squads) configured in server-config.json with response status code 200', async () => {
+    const response = await request(app).get('/api/v1/metrics/client-filters');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(ServerConfiguration.clientFilters);
+  });
+});
