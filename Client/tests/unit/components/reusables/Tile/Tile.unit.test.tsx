a5219d31d21760b911cf34b85bee4128e41f451b
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-03-01 11:56:41 +0530

    refactor: resolve reviewer comments.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- Client/tests/unit/components/reusables/Tile/Tile.unit.test.tsx
+++ Client/tests/unit/components/reusables/Tile/Tile.unit.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 
 import { Tile } from "../../../../../src/components/reusables/Tile/Tile";
 
-const MockChildComponent = () => <div>Test Children</div>;
+const ChildComponent = () => <div>Test Children</div>;
 
 describe("Tile component - render tile with title & children", () => {
   it("should renders the tile's title and children", () => {
     render(
       <Tile title="Test Title">
-        <MockChildComponent />
+        <ChildComponent />
       </Tile>,
     );
 
