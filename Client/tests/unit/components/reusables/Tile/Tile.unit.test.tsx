39c72faf3289d41aca4030faabd304f96071a66b
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-03-01 16:45:23 +0530

    FE | Refactor tests & configs (#25)
    * refactor: rename file names.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: change the description.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: modify playwright config.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: resolve PR comments.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    ---------
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- Client/tests/unit/components/reusables/Tile/Tile.unit.test.tsx
+++ Client/tests/unit/components/reusables/Tile/Tile.unit.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
 
 import { Tile } from "../../../../../src/components/reusables/Tile/Tile";
 
 const ChildComponent = () => <div>Test Children</div>;
 
-describe("Tile component - render tile with title & children", () => {
-  it("should renders the tile's title and children", () => {
+describe("Tile component", () => {
+  it("should render the tile's title and children", () => {
     render(
       <Tile title="Test Title">
         <ChildComponent />
