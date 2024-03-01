95e0ea988e01d8e957ab76f7766c544a63308c0f
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-03-01 19:45:27 +0530

    FE | Bug fix client filters scroll bar. (#27)
    * bug-fix: fix scroll bar for client filters.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: change path for vitest coverage.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    ---------
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
-      reportsDirectory: "./src/test/coverage",
+      reportsDirectory: "./tests/coverage",
     },
   },
 });
