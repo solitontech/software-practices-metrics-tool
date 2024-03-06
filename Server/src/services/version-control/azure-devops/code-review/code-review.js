eaaaef1297a2c54c14719ada4bd1919f74ade5bd
Sathish Thangavel <sathish.thangavel@solitontech.local>
2024-03-06 22:08:14 +0530

    refactor: code review clean up

--- Server/src/services/version-control/azure-devops/code-review/code-review.js
+++ Server/src/services/version-control/azure-devops/code-review/code-review.js
@@ -7,19 +7,106 @@ import { AzureDevopsURL } from '../helpers/index.js';
 import { CODE_TO_VOTE, COMMENT_TYPE } from './constants/index.js';
 
 export class CodeReview {
-  static voteResult = 'CodeReviewVoteResult';
+  static #parseReviewers(rawReviewers) {
+    // TODO: convert to map if ot needed
+    const reviewers = {};
+
+    rawReviewers.forEach(({ id, displayName, vote, isRequired }) => {
+      reviewers[id] = {
+        author: displayName,
+        vote: CODE_TO_VOTE.get(vote),
+        isRequired: isRequired ?? false,
+      };
+    });
+
+    return reviewers;
+  }
+
+  static #parseVotesTimeline(pullRequestThreads) {
+    const votes = [];
+    const voteResult = 'CodeReviewVoteResult';
+
+    pullRequestThreads.forEach((thread) => {
+      if (!thread.properties) {
+        return;
+      }
+
+      const vote = thread.properties[voteResult];
+      const [firstComment] = thread.comments;
+      const isNoVote = (vote) => {
+        return !parseInt(vote.$value);
+      };
+
+      if (vote && !isNoVote(vote)) {
+        votes.push({
+          author: firstComment.author.displayName,
+          id: firstComment.author.id,
+          timeOfVote: thread.publishedDate,
+          vote: CODE_TO_VOTE.get(parseInt(vote.$value)),
+        });
+      }
+    });
+
+    return votes;
+  }
+
+  static #parseThreads(pullRequestThreads) {
+    const threads = [];
+
+    pullRequestThreads.forEach((thread) => {
+      const [firstComment] = thread.comments;
+      const isValidThread = thread.pullRequestThreadContext ?? firstComment?.commentType === COMMENT_TYPE.STRING;
+
+      if (thread.isDeleted || !isValidThread) {
+        return;
+      }
+
+      const comments = thread.comments.map(({ content, author: { displayName, id } }) => {
+        return { content: content ?? '', authorName: displayName, authorId: id };
+      });
+
+      threads.push({ comments });
+    });
+
+    return threads;
+  }
+
+  static #parseTags(pullRequest) {
+    if (!pullRequest.labels) {
+      return [];
+    }
+
+    return pullRequest.labels.map((label) => {
+      return label.name;
+    });
+  }
+
+  static #parsePullRequests(pullRequests) {
+    return pullRequests.map((pullRequest) => {
+      return {
+        id: pullRequest.pullRequestId,
+        title: pullRequest.title,
+        status: pullRequest.status,
+        createdBy: pullRequest.createdBy.displayName,
+        authorId: pullRequest.createdBy.id,
+        creationDate: pullRequest.creationDate,
+        closedDate: pullRequest.closedDate,
+        reviewers: this.#parseReviewers(pullRequest.reviewers),
+        votesHistoryTimeline: this.#parseVotesTimeline(pullRequest.threads),
+        threads: this.#parseThreads(pullRequest.threads),
+        tags: this.#parseTags(pullRequest),
+      };
+    });
+  }
+
+  static #isRequiredReviewersAssigned(reviewers) {
+    return reviewers.some((reviewer) => reviewer.isRequired);
+  }
 
   static getCodeReviewMetrics(rawPullRequests) {
-    const codeReviewMetrics = {
-      count: rawPullRequests.length,
-      pullRequests: [],
-    };
-
-    const pullRequests = this.parsePullRequests(rawPullRequests);
-
-    codeReviewMetrics.pullRequests = pullRequests.map((pullRequest) => {
+    const pullRequests = this.#parsePullRequests(rawPullRequests).map((pullRequest) => {
       const reviewers = Object.values(pullRequest.reviewers);
-      const isRequiredReviewers = this.isRequiredReviewersAssigned(reviewers);
+      const isRequiredReviewers = this.#isRequiredReviewersAssigned(reviewers);
 
       return {
         id: pullRequest.id,
@@ -44,100 +131,9 @@ export class CodeReview {
       };
     });
 
-    return codeReviewMetrics;
-  }
-
-  static parsePullRequests(pullRequests) {
-    return pullRequests.map((pullRequest) => {
-      return {
-        id: pullRequest.pullRequestId,
-        title: pullRequest.title,
-        status: pullRequest.status,
-        createdBy: pullRequest.createdBy.displayName,
-        authorId: pullRequest.createdBy.id,
-        creationDate: pullRequest.creationDate,
-        closedDate: pullRequest.closedDate,
-        reviewers: this.parseReviewers(pullRequest.reviewers),
-        votesHistoryTimeline: this.parseVotesTimeline(pullRequest.threads),
-        threads: this.parseThreads(pullRequest.threads),
-        tags: this.parseTags(pullRequest),
-      };
-    });
-  }
-
-  static parseReviewers(rawReviewers) {
-    const reviewers = {};
-
-    rawReviewers.forEach((reviewer) => {
-      reviewers[reviewer.id] = {
-        author: reviewer.displayName,
-        vote: CODE_TO_VOTE.get(reviewer.vote),
-        isRequired: reviewer.isRequired ?? false,
-      };
-    });
-
-    return reviewers;
-  }
-
-  static parseVotesTimeline(pullRequestThreads) {
-    const votes = [];
-
-    pullRequestThreads.forEach((thread) => {
-      if (!thread.properties) {
-        return;
-      }
-
-      const vote = thread.properties[this.voteResult];
-      const [firstComment] = thread.comments;
-      const isNoVote = (vote) => {
-        return !parseInt(vote.$value);
-      };
-
-      if (vote && !isNoVote(vote)) {
-        votes.push({
-          author: firstComment.author.displayName,
-          id: firstComment.author.id,
-          timeOfVote: thread.publishedDate,
-          vote: CODE_TO_VOTE.get(parseInt(vote.$value)),
-        });
-      }
-    });
-
-    return votes;
-  }
-
-  static parseThreads(pullRequestThreads) {
-    const threads = [];
-
-    pullRequestThreads.forEach((thread) => {
-      const [firstComment] = thread.comments;
-      const isValidThread = thread.pullRequestThreadContext ?? firstComment?.commentType === COMMENT_TYPE.STRING;
-
-      if (thread.isDeleted || !isValidThread) {
-        return;
-      }
-
-      const comments = thread.comments.map(({ content, author: { displayName, id } }) => {
-        return { content: content ?? '', authorName: displayName, authorId: id };
-      });
-
-      threads.push({ comments });
-    });
-
-    return threads;
-  }
-
-  static isRequiredReviewersAssigned(reviewers) {
-    return reviewers.some((reviewer) => reviewer.isRequired);
-  }
-
-  static parseTags(pullRequest) {
-    if (!pullRequest.labels) {
-      return [];
-    }
-
-    return pullRequest.labels.map((label) => {
-      return label.name;
-    });
+    return {
+      count: rawPullRequests.length,
+      pullRequests,
+    };
   }
 }
