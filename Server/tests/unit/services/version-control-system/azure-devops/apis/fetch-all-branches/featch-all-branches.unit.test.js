29d7be57bfeb4b0193376ed931a63598959f070e
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-03-06 20:57:04 +0530

    refactor: make specs clean.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- Server/tests/unit/services/version-control-system/azure-devops/apis/fetch-all-branches/featch-all-branches.unit.test.js
+++ Server/tests/unit/services/version-control-system/azure-devops/apis/fetch-all-branches/featch-all-branches.unit.test.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import btoa from 'btoa';
-
 import { jest, describe, it, expect, afterEach } from '@jest/globals';
-import { ALL_BRANCHES_AZURE_RESPONSE } from './fetch-all-branches.mock.js';
+
 import { AzureDevopsApi } from '../../../../../../../src/services/version-control/azure-devops/apis/azure-devops.api.js';
 import { AppError } from '../../../../../../../src/utils/app-error.js';
 import { ServerConfiguration } from '../../../../../../../src/configs/server.config.js';
+
+import { ALL_BRANCHES_AZURE_RESPONSE } from './fetch-all-branches.mock.js';
+import { STATUS_CODE } from '../../../../../../../src/constants/http-status-code.constant.js';
 
 jest.mock('axios');
 
@@ -15,102 +17,84 @@ const {
   repositoryId: REPOSITORY_ID,
   authToken: TOKEN,
 } = ServerConfiguration.versionControl;
+
 const HEADER = ':' + TOKEN;
+
+const AXIOS_REQUEST_PARAMETERS = [
+  `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/git/repositories/${REPOSITORY_ID}/refs?filter=heads&api-version=7.1-preview.1`,
+  {
+    headers: {
+      Authorization: `Basic ${btoa(HEADER)}`,
+      'Content-Type': 'application/json',
+    },
+  },
+];
 
 describe('AzureDevopsApi~fetchAllBranches - returns all branches for repository', () => {
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
-  it('should fetch all branches successfully when data is present', async () => {
-    const mockResponse = { data: ALL_BRANCHES_AZURE_RESPONSE, status: 200 };
+  it('should fetch all branches successfully when data is present in azure repository', async () => {
+    const mockResponse = { data: ALL_BRANCHES_AZURE_RESPONSE, status: STATUS_CODE.OK };
+
     axios.get = jest.fn().mockResolvedValue(mockResponse);
 
     const response = await AzureDevopsApi.fetchAllBranches();
 
     expect(response).toEqual(ALL_BRANCHES_AZURE_RESPONSE);
-    expect(axios.get).toHaveBeenCalledWith(
-      `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/git/repositories/${REPOSITORY_ID}/refs?filter=heads&api-version=7.1-preview.1`,
-      {
-        headers: {
-          Authorization: `Basic ${btoa(HEADER)}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    expect(axios.get).toHaveBeenCalledWith(...AXIOS_REQUEST_PARAMETERS);
   });
 
-  it('should throw AppError with status 404 when no branches are found', async () => {
-    const mockResponse = { data: { value: [], count: 0 }, status: 200 };
+  it('should throw AppError with status 404 when no branches are found in azure repository', async () => {
+    const mockResponse = { data: { value: [], count: 0 }, status: STATUS_CODE.OK };
+
     axios.get = jest.fn().mockResolvedValue(mockResponse);
     jest.spyOn(AppError, 'throwAppError');
 
     const response = AzureDevopsApi.fetchAllBranches();
 
     await expect(response).rejects.toThrow(AppError);
-    expect(AppError.throwAppError).toHaveBeenCalledWith(AzureDevopsApi.dataNotFound, 404);
-    expect(axios.get).toHaveBeenCalledWith(
-      `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/git/repositories/${REPOSITORY_ID}/refs?filter=heads&api-version=7.1-preview.1`,
-      {
-        headers: {
-          Authorization: `Basic ${btoa(HEADER)}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    expect(AppError.throwAppError).toHaveBeenCalledWith(AzureDevopsApi.dataNotFound, STATUS_CODE.NOT_FOUND);
+    expect(axios.get).toHaveBeenCalledWith(...AXIOS_REQUEST_PARAMETERS);
   });
 
-  it('should throw AppError with status 404 when request fails due to 404', async () => {
-    axios.get = jest.fn().mockRejectedValue({ response: { status: 404 } });
+  it('should throw AppError with status 404 when request to azure api fails due to 404', async () => {
+    axios.get = jest.fn().mockRejectedValue({ response: { status: STATUS_CODE.NOT_FOUND } });
     jest.spyOn(AppError, 'throwAppError');
 
     const response = AzureDevopsApi.fetchAllBranches();
+
     await expect(response).rejects.toThrow(AppError);
-    expect(AppError.throwAppError).toHaveBeenCalledWith(AzureDevopsApi.invalidRepositoryDetails, 404);
-    expect(axios.get).toHaveBeenCalledWith(
-      `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/git/repositories/${REPOSITORY_ID}/refs?filter=heads&api-version=7.1-preview.1`,
-      {
-        headers: {
-          Authorization: `Basic ${btoa(HEADER)}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
+    expect(AppError.throwAppError).toHaveBeenCalledWith(AzureDevopsApi.invalidRepositoryDetails, STATUS_CODE.NOT_FOUND);
+    expect(axios.get).toHaveBeenCalledWith(...AXIOS_REQUEST_PARAMETERS);
   });
 
-  it('should throw AppError with status 401 when request fails due to 401', async () => {
-    axios.get = jest.fn().mockRejectedValue({ response: { status: 401 } });
+  it('should throw AppError with status 401 when request to azure fails due to 401', async () => {
+    axios.get = jest.fn().mockRejectedValue({ response: { status: STATUS_CODE.UNAUTHORIZED_ACCESS } });
     jest.spyOn(AppError, 'throwAppError');
 
     const response = AzureDevopsApi.fetchAllBranches();
+
     await expect(response).rejects.toThrow(AppError);
-    expect(AppError.throwAppError).toHaveBeenCalledWith(AzureDevopsApi.invalidAzureToken, 401);
-    expect(axios.get).toHaveBeenCalledWith(
-      `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/git/repositories/${REPOSITORY_ID}/refs?filter=heads&api-version=7.1-preview.1`,
-      {
-        headers: {
-          Authorization: `Basic ${btoa(HEADER)}`,
-          'Content-Type': 'application/json',
-        },
-      }
+    expect(AppError.throwAppError).toHaveBeenCalledWith(
+      AzureDevopsApi.invalidAzureToken,
+      STATUS_CODE.UNAUTHORIZED_ACCESS
     );
+    expect(axios.get).toHaveBeenCalledWith(...AXIOS_REQUEST_PARAMETERS);
   });
 
-  it('should throw AppError with status 401 when request fails due to 203', async () => {
-    axios.get = jest.fn().mockResolvedValue({ data: { count: 0, value: [] }, status: 203 });
+  it('should throw AppError with status 401 when request to azure fails due to 203', async () => {
+    axios.get = jest.fn().mockResolvedValue({ status: STATUS_CODE.NON_AUTHORITATIVE_INFORMATION });
     jest.spyOn(AppError, 'throwAppError');
 
     const response = AzureDevopsApi.fetchAllBranches();
+
     await expect(response).rejects.toThrow(AppError);
-    expect(AppError.throwAppError).toHaveBeenCalledWith(AzureDevopsApi.invalidAzureToken, 401);
-    expect(axios.get).toHaveBeenCalledWith(
-      `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/git/repositories/${REPOSITORY_ID}/refs?filter=heads&api-version=7.1-preview.1`,
-      {
-        headers: {
-          Authorization: `Basic ${btoa(HEADER)}`,
-          'Content-Type': 'application/json',
-        },
-      }
+    expect(AppError.throwAppError).toHaveBeenCalledWith(
+      AzureDevopsApi.invalidAzureToken,
+      STATUS_CODE.UNAUTHORIZED_ACCESS
     );
+    expect(axios.get).toHaveBeenCalledWith(...AXIOS_REQUEST_PARAMETERS);
   });
 });
