2d9988d84081c91cb83d90ce694fbf4b2c751819
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-03-07 10:57:46 +0530

    BE | Add unit tests for all branches method in azure devops apis. (#33)
    * feat: add unit tests for all branches.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: make specs clean.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: resolve PR comments.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: resolve PR comments.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    ---------
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

---
+++ Server/tests/unit/services/version-control-system/azure-devops/apis/fetch-all-branches/fetch-all-branches.mock.js
@@ -0,0 +1,33 @@
+export const ALL_BRANCHES_AZURE_RESPONSE = {
+  count: 2,
+  value: [
+    {
+      name: 'refs/heads/25Automation',
+      objectId: 'a30a4b19037819eed3ded296ae074f7443f803f2',
+      creator: {
+        displayName: 'Developer 1',
+        url: 'https://spsprodcin2.vssps.visualstudio.com/A64933cd1-34af-45f4-8a18-45cb982e7998/_apis/Identities/d2145d45-4253-6f16-9688-391s21a6f728',
+        id: 'a2445d45-4253-6f16-9682-391c21a6f768',
+        uniqueName: 'developer.1@solitontech.com',
+        imageUrl:
+          'https://dev.azure.com/${ORGANIZATION}/_api/_common/identityImage?id=d2745d45-4253-6f16-9688-391c21a6f768',
+        descriptor: 'aad.ZDI3NDVkNDUtNDI1My03ZiE1LTk2ODgtMzkxYzIxYTZmNzY4',
+      },
+      url: 'https://dev.azure.com/${ORGANIZATION}/66c3016e-9f23-4861-bdd8-81be4d509c2e/_apis/git/repositories/fe7b3b03-e25f-48d5-97be-19759b2f010b/refs?filter=heads%25Automation',
+    },
+    {
+      name: 'refs/heads/react/starter/code',
+      objectId: '736f4f3ec1fc3159534cedc7bc3272dd2f1f7f93',
+      creator: {
+        displayName: 'Developer 2',
+        url: 'https://spsprodcin2.vssps.visualstudio.com/A64933cd1-34af-45f4-8a18-45cb982e7998/_apis/Identities/d2145d45-4253-6f16-9688-391s21a6f728',
+        id: 'a2445d45-4253-6f16-9682-391c21a6f768',
+        uniqueName: 'developer.2@solitontech.com',
+        imageUrl:
+          'https://dev.azure.com/${ORGANIZATION}/_api/_common/identityImage?id=d2745d45-4253-6f16-9688-391c21a6f768',
+        descriptor: 'aad.ZDI3NDVkNDUtNDI1My03ZiE1LTk2ODgtMzkxYzIxYTZmNzY4',
+      },
+      url: 'https://dev.azure.com/${ORGANIZATION}/66c3016e-9f23-4861-bdd8-81be4d509c2e/_apis/git/repositories/fe7b3b03-e25f-48d5-97be-19759b2f010b/refs?filter=heads%react/starter/code',
+    },
+  ],
+};
