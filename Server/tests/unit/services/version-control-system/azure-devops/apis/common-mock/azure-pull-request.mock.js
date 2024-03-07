fceede2f4a2b441adb317509a1aa4a2bf5c144f8
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-03-07 11:22:37 +0530

    BE | Add unit tests for other pull request methods of azure devops api. (#36)
    * feat: add unit tests for fetch pull requests method.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: make code clean.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: rearrange imports.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * feat: add unit tests for other pull request methods.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: modify descriptions.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: resolve PR comments.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * remove dependency on pull request list.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: change the directory and file name for common mocks.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    ---------
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

---
+++ Server/tests/unit/services/version-control-system/azure-devops/apis/common-mock/azure-pull-request.mock.js
@@ -0,0 +1,56 @@
+export const AZURE_PULL_REQUESTS_RESPONSE = {
+  count: 3,
+  value: [
+    {
+      pullRequestId: 1,
+      status: 'active',
+      createdBy: {
+        displayName: 'Developer',
+        id: '9e1313a9-2b7c-6556-b441-d1eabfed5d43',
+      },
+      creationDate: '2024-02-04T10:58:33.4719437Z',
+      title: 'Only the reviewers team is required to vote & a member voted for approved.',
+      reviewers: [
+        {
+          vote: 10,
+          displayName: 'Reviewer 1',
+          id: '6f96f2d9-f079-65fd-ae3d-0650c193e580',
+        },
+      ],
+    },
+    {
+      pullRequestId: 2,
+      status: 'active',
+      createdBy: {
+        displayName: 'Developer',
+        id: '9e1313a9-2b7c-6556-b441-d1eabfed5d43',
+      },
+      creationDate: '2024-02-04T10:58:33.4719437Z',
+      title: 'Only the reviewers team is required to vote & a member voted for approved.',
+      reviewers: [
+        {
+          vote: 10,
+          displayName: 'Reviewer 1',
+          id: '6f96f2d9-f079-65fd-ae3d-0650c193e580',
+        },
+      ],
+    },
+    {
+      pullRequestId: 3,
+      status: 'active',
+      createdBy: {
+        displayName: 'Developer',
+        id: '9e1313a9-2b7c-6556-b331-e1eabfed5d43',
+      },
+      creationDate: '2024-02-04T10:58:33.4719437Z',
+      title: 'Only the reviewers team is required to vote & a member voted for approved.',
+      reviewers: [
+        {
+          vote: 10,
+          displayName: 'Reviewer 1',
+          id: '6f96f2d9-f079-65fd-ae3d-0650c193e580',
+        },
+      ],
+    },
+  ],
+};
