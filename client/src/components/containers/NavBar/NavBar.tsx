6055295541e746b25e52cbb55d4281bdef58ae2c
Sathish Thangavel <sathish.thangavel@solitontech.local>
2024-03-13 18:05:15 +0530

    refactor: navbar

--- client/src/components/containers/NavBar/NavBar.tsx
+++ client/src/components/containers/NavBar/NavBar.tsx
@@ -1,91 +1,56 @@
 import { useState } from "react";
 
-import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
-import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
-import { Drawer } from "@mui/material";
+import CodeOutlinedIcon from "@mui/icons-material/CodeOutlined";
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
 
-import styles from "./NavBar.module.scss";
-import { routes } from "./navBarConstants.tsx";
-import logo from "../../../assets/images/solitonWhiteLogo.svg";
-import techForceLogo from "../../../assets/images/techForceLogo.png";
+import trunkBased from "src/assets/images/trunkBased.svg";
+import { routePaths } from "src/constants";
 
-export interface Route {
-  name: string;
-  label: string;
-  link: string;
-  iconPath: React.ReactNode;
-}
+import styles from "./NavBar.module.scss";
+import { NavBarCollapsed } from "./NavBarCollapsed/NavBarCollapsed.tsx";
+import { NavBarExpanded } from "./NavBarExpanded/NavBarExpanded.tsx";
+
+const routes = [
+  {
+    name: "Code review Metrics",
+    label: "Code review",
+    link: routePaths.codeReviewMetrics,
+    icon: <CodeOutlinedIcon />,
+  },
+  {
+    name: "Trunk Based Metrics",
+    label: "Trunk Based",
+    link: routePaths.trunkBasedMetrics,
+    icon: <img src={trunkBased} />,
+  },
+];
 
 export const NavBar = () => {
   const [isNavBarOpen, setIsNavBarOpen] = useState(false);
 
-  const getListItems = (routes: Route[], isLabelNeeded: boolean) => {
-    return routes.map((route) => (
-      <NavLink
-        className={(navData) =>
-          `${isLabelNeeded ? styles.link : styles.smallLink}  ${navData.isActive ? styles.selectedPage : ""} `
-        }
-        to={route.link}
-        key={route.label}
-        title={isLabelNeeded ? "" : route.label}
-      >
-        <div className={isLabelNeeded ? styles.pageIconCtr : styles.smallPageIconCtr}>
-          <span>{route.iconPath}</span>
-        </div>
-        {isLabelNeeded ? <p>{route.label}</p> : ""}
-      </NavLink>
-    ));
+  const linkClass = isNavBarOpen ? styles.expandedLink : styles.collapsedLink;
+  const iconClass = isNavBarOpen ? styles.expandedIcon : styles.collapsedIcon;
+
+  const navRoutes = routes.map((route) => (
+    <NavLink
+      className={({ isActive }) => `${linkClass} ${clsx(isActive && styles.activePage)}`}
+      to={route.link}
+      key={route.label}
+      title={route.label}
+    >
+      <div className={iconClass}>{route.icon}</div>
+      {isNavBarOpen && <p>{route.label}</p>}
+    </NavLink>
+  ));
+
+  const toggleNavBar = () => {
+    setIsNavBarOpen(!isNavBarOpen);
   };
 
-  return (
-    <>
-      {isNavBarOpen ? (
-        <div className={styles.navBar}>
-          <Drawer
-            variant="permanent"
-            classes={{
-              paper: styles.drawerPaper,
-            }}
-            anchor="left"
-            style={{ height: "100%" }}
-          >
-            <div className={styles.drawerToolbar}>
-              <img src={logo} alt="Soliton"></img>
-              <div className={styles.cursorPointer} onClick={() => setIsNavBarOpen(!isNavBarOpen)}>
-                <ArrowCircleLeftOutlinedIcon className={styles.icon} />
-              </div>
-            </div>
-            <div className={` ${styles.utilizeAvailableSpace}`}>{getListItems(routes, true)}</div>
-            <div className={styles.techForce}>
-              <div>
-                <img src={techForceLogo} alt="logo" />
-              </div>
-            </div>
-            <p className={styles.logoText}>Powered by TechOps</p>
-          </Drawer>
-        </div>
-      ) : (
-        <div>
-          <Drawer
-            variant="permanent"
-            classes={{
-              paper: styles.smallDrawerPaper,
-            }}
-            anchor="left"
-            style={{ height: "100%" }}
-          >
-            <div className={styles.smallDrawerContent}>
-              <div onClick={() => setIsNavBarOpen(!isNavBarOpen)} className={styles.cursorPointer}>
-                <ArrowCircleRightOutlinedIcon className={styles.icon} />
-              </div>
-              <div>
-                <div className={` ${styles.utilizeAvailableSpace}`}>{getListItems(routes, false)}</div>
-              </div>
-            </div>
-          </Drawer>
-        </div>
-      )}
-    </>
-  );
+  if (isNavBarOpen) {
+    return <NavBarExpanded routes={navRoutes} handleCollapse={toggleNavBar} />;
+  }
+
+  return <NavBarCollapsed routes={navRoutes} handleExpand={toggleNavBar} />;
 };
