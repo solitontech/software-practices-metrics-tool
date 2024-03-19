18de80506fe7a6aac0c4f78229890bff7cba7c26
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-03-19 11:05:28 +0530

    fix: resolve console errors. (#58)
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- client/src/components/reusables/Tile/Tile.tsx
+++ client/src/components/reusables/Tile/Tile.tsx
@@ -9,7 +9,7 @@ export const Tile = ({ title, children }: ITileProps) => {
   return (
     <div className={styles.tile}>
       <h6 className={styles.title}>{title}</h6>
-      <p className={styles.children}>{children}</p>
+      {children}
     </div>
   );
 };
