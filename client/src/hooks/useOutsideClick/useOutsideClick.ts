c09a19992853a7298fec108da9a3ac462bdd5a7d
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-03-21 13:29:30 +0530

    refactor: change variable name.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- client/src/hooks/useOutsideClick/useOutsideClick.ts
+++ client/src/hooks/useOutsideClick/useOutsideClick.ts
@@ -1,15 +1,15 @@
 import { RefObject, useEffect } from "react";
 
-export const useOutsideClick = (ref: RefObject<HTMLElement>, setIsVisible: (isVisible: boolean) => void) => {
+export const useOutsideClick = (ref: RefObject<HTMLElement>, setIsInside: (isInside: boolean) => void) => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (event.target instanceof Node && ref.current && !ref.current.contains(event.target)) {
-        setIsVisible(false);
+        setIsInside(false);
       }
     };
 
     document.addEventListener("mousedown", handleClickOutside);
 
     return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, [ref, setIsVisible]);
+  }, [ref, setIsInside]);
 };
