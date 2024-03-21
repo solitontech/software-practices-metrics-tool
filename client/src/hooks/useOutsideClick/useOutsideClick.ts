fa7578558d4f8f0769ad60a274dcd1ad7dd2fedc
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-03-21 13:30:57 +0530

    refactor: change function name.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- client/src/hooks/useOutsideClick/useOutsideClick.ts
+++ client/src/hooks/useOutsideClick/useOutsideClick.ts
@@ -2,14 +2,14 @@ import { RefObject, useEffect } from "react";
 
 export const useOutsideClick = (ref: RefObject<HTMLElement>, setIsInside: (isInside: boolean) => void) => {
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+    const handleOutsideClick = (event: MouseEvent) => {
       if (event.target instanceof Node && ref.current && !ref.current.contains(event.target)) {
         setIsInside(false);
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleOutsideClick);
 
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
   }, [ref, setIsInside]);
 };
