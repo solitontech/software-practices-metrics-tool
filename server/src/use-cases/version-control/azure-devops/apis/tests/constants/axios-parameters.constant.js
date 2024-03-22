370d3d54173a7b128c6bf4cd6ad8f9e512efd54b
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-03-22 11:14:42 +0530

    refactor: change target branch dynamic in axios params.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- server/src/use-cases/version-control/azure-devops/apis/tests/constants/axios-parameters.constant.js
+++ server/src/use-cases/version-control/azure-devops/apis/tests/constants/axios-parameters.constant.js
@@ -8,6 +8,7 @@ export const START_DATE = '2022-01-01';
 export const END_DATE = '2022-12-31';
 
 const {
+  targetBranch: TARGET_BRANCH,
   organization: ORGANIZATION,
   projectName: PROJECT,
   repositoryId: REPOSITORY_ID,
@@ -19,19 +20,19 @@ export const BASE_URL = `https://dev.azure.com/${ORGANIZATION}/${PROJECT}/_apis/
 
 const FETCH_ALL_BRANCHES_MAIN_URL = `refs?filter=heads&api-version=7.1-preview.1`;
 
-const FETCH_ACTIVE_PULL_REQUESTS_MAIN_URL = `pullRequests?searchCriteria.status=active&searchCriteria.targetRefName=refs%2Fheads%2Fmain&%24top=${PAGE_SIZE}&%24skip=${
+const FETCH_ACTIVE_PULL_REQUESTS_MAIN_URL = `pullRequests?searchCriteria.status=active&searchCriteria.targetRefName=refs%2Fheads%2F${TARGET_BRANCH}&%24top=${PAGE_SIZE}&%24skip=${
   PAGE - 1
 }&api-version=7.1-preview.1`;
 
-const FETCH_COMMITS_LIST_MAIN_URL = `commits?searchCriteria.itemVersion.version=main&searchCriteria.fromDate=${START_DATE}&searchCriteria.toDate=${END_DATE}&api-version=7.1-preview.1&%24top=${PAGE_SIZE}&%24skip=${
+const FETCH_COMMITS_LIST_MAIN_URL = `commits?searchCriteria.itemVersion.version=${TARGET_BRANCH}&searchCriteria.fromDate=${START_DATE}&searchCriteria.toDate=${END_DATE}&api-version=7.1-preview.1&%24top=${PAGE_SIZE}&%24skip=${
   PAGE - 1
 }`;
 
-const FETCH_PULL_REQUESTS_MAIN_URL = `pullRequests?searchCriteria.status=all&searchCriteria.targetRefName=refs%2Fheads%2Fmain&searchCriteria.queryTimeRangeType=created&searchCriteria.minTime=${START_DATE}&searchCriteria.maxTime=${END_DATE}&%24top=${PAGE_SIZE}&%24skip=${
+const FETCH_PULL_REQUESTS_MAIN_URL = `pullRequests?searchCriteria.status=all&searchCriteria.targetRefName=refs%2Fheads%2F${TARGET_BRANCH}&searchCriteria.queryTimeRangeType=created&searchCriteria.minTime=${START_DATE}&searchCriteria.maxTime=${END_DATE}&%24top=${PAGE_SIZE}&%24skip=${
   PAGE - 1
 }&api-version=7.1-preview.1`;
 
-const FETCH_PULL_REQUESTS_LIST_MAIN_URL = `pullRequests?searchCriteria.status=all&searchCriteria.targetRefName=refs%2Fheads%2Fmain&searchCriteria.queryTimeRangeType=created&searchCriteria.minTime=${START_DATE}&searchCriteria.maxTime=${END_DATE}&%24top=${PAGE_SIZE}&%24skip=${
+const FETCH_PULL_REQUESTS_LIST_MAIN_URL = `pullRequests?searchCriteria.status=all&searchCriteria.targetRefName=refs%2Fheads%2F${TARGET_BRANCH}&searchCriteria.queryTimeRangeType=created&searchCriteria.minTime=${START_DATE}&searchCriteria.maxTime=${END_DATE}&%24top=${PAGE_SIZE}&%24skip=${
   PAGE - 1
 }&api-version=7.1-preview.1`;
 
