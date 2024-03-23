0ffdd7b62fc4a9c1b2b78a3d7c2895104ff8e882
Sathish Thangavel <sathish.thangavel@solitontech.local>
2024-03-23 17:37:26 +0530

    refactor: table mixin

--- client/src/components/containers/TrunkBasedMetricsContainers/TrunkBasedPullRequestsTable/trunkBasedPullRequestsTableConstants.ts
+++ client/src/components/containers/TrunkBasedMetricsContainers/TrunkBasedPullRequestsTable/trunkBasedPullRequestsTableConstants.ts
@@ -4,25 +4,25 @@ export const tableColumns: ITrunkBasedPullRequestsTableColumn[] = [
   {
     id: "creationDate",
     label: "Start Date",
-    width: 150,
+    width: 110,
     align: "left",
   },
   {
     id: "closedDate",
     label: "End Date",
-    width: 150,
+    width: 110,
     align: "left",
   },
   {
     id: "name",
     label: "Branch Name",
-    width: 350,
+    width: 430,
     align: "left",
   },
   {
     id: "title",
     label: "Pull Request Title",
-    width: 350,
+    width: 430,
     align: "left",
   },
   {
