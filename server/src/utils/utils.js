181582471c980c92f8219cf841440edb788d9a0a
ganesh-nithin <127190944+ganesh-nithin@users.noreply.github.com>
2024-03-27 16:12:57 +0530

    Refactor: follow naming conventions for tool. (#84)
    * refactor: follow naming conventions for tool.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    * refactor: resolve CI confilcts.
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>
    
    ---------
    
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- server/src/utils/utils.js
+++ server/src/utils/utils.js
@@ -1,4 +1,4 @@
+export * from './server-config.util.js';
 export * from './app-error.util.js';
 export * from './catch-async.util.js';
 export * from './logger.util.js';
-export * from './server-config.util.js';
