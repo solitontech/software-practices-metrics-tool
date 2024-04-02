672d31582fbf0d49b87b093fe6f8a45081de2da4
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-04-02 19:49:01 +0530

    test: use star import for importing mock data.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- server/src/use-cases/version-control/azure-devops/code-review/code-review.unit.test.js
+++ server/src/use-cases/version-control/azure-devops/code-review/code-review.unit.test.js
@@ -1,47 +1,36 @@
 import { describe, it, expect } from '@jest/globals';
 
 import { CodeReview } from './code-review.js';
-import {
-  CODE_REVIEW_METRICS,
-  CODE_REVIEW_METRICS_THREADS,
-  CODE_REVIEW_NO_VOTE_THREADS,
-  CODE_REVIEW_WITHOUT_CLOSED_DATE,
-  CODE_REVIEW_WITHOUT_TAGS,
-  RAW_CODE_REVIEW_METRICS,
-  RAW_CODE_REVIEW_METRICS_THREADS,
-  RAW_CODE_REVIEW_NO_VOTE_THREADS,
-  RAW_CODE_REVIEW_WITHOUT_CLOSED_DATE,
-  RAW_CODE_REVIEW_WITHOUT_LABELS,
-} from './tests/code-review.mock.js';
+import * as mock from './tests/code-review.mock.js';
 
 describe('CodeReview~getCodeReviewMetrics', () => {
   it('should return the code review metrics', () => {
-    const result = CodeReview.getCodeReviewMetrics(RAW_CODE_REVIEW_METRICS);
+    const result = CodeReview.getCodeReviewMetrics(mock.RAW_CODE_REVIEW_METRICS);
 
-    expect(result).toEqual(CODE_REVIEW_METRICS);
+    expect(result).toEqual(mock.CODE_REVIEW_METRICS);
   });
 
   it('should return code review metrics with closed date as null if the pull request not contain closed date', () => {
-    const result = CodeReview.getCodeReviewMetrics(RAW_CODE_REVIEW_WITHOUT_CLOSED_DATE);
+    const result = CodeReview.getCodeReviewMetrics(mock.RAW_CODE_REVIEW_WITHOUT_CLOSED_DATE);
 
-    expect(result).toEqual(CODE_REVIEW_WITHOUT_CLOSED_DATE);
+    expect(result).toEqual(mock.CODE_REVIEW_WITHOUT_CLOSED_DATE);
   });
 
   it('should return code review metrics with empty votes timeline if the pull request not contain votes threads', () => {
-    const result = CodeReview.getCodeReviewMetrics(RAW_CODE_REVIEW_NO_VOTE_THREADS);
+    const result = CodeReview.getCodeReviewMetrics(mock.RAW_CODE_REVIEW_NO_VOTE_THREADS);
 
-    expect(result).toEqual(CODE_REVIEW_NO_VOTE_THREADS);
+    expect(result).toEqual(mock.CODE_REVIEW_NO_VOTE_THREADS);
   });
 
   it('should return code review metrics by omitting deleted threads & comments', () => {
-    const result = CodeReview.getCodeReviewMetrics(RAW_CODE_REVIEW_METRICS_THREADS);
+    const result = CodeReview.getCodeReviewMetrics(mock.RAW_CODE_REVIEW_METRICS_THREADS);
 
-    expect(result).toEqual(CODE_REVIEW_METRICS_THREADS);
+    expect(result).toEqual(mock.CODE_REVIEW_METRICS_THREADS);
   });
 
   it('should return pull requests with out tags if the raw pull request not contain labels', () => {
-    const result = CodeReview.getCodeReviewMetrics(RAW_CODE_REVIEW_WITHOUT_LABELS);
+    const result = CodeReview.getCodeReviewMetrics(mock.RAW_CODE_REVIEW_WITHOUT_LABELS);
 
-    expect(result).toEqual(CODE_REVIEW_WITHOUT_TAGS);
+    expect(result).toEqual(mock.CODE_REVIEW_WITHOUT_TAGS);
   });
 });
