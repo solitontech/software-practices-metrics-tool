1a03d27892de35f46d370ed527ae9573d4756237
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-04-02 11:22:47 +0530

    feat: add unit test cases for helper utils.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

---
+++ server/src/use-cases/version-control/azure-devops/helpers/date.helpers.unit.test.js
@@ -0,0 +1,23 @@
+import { describe, it, expect } from '@jest/globals';
+
+import { getGmtISOString, getNextDayGmtISOString } from './date.helpers.js';
+
+describe('getGmtISOString - method to return GMT date string from IST date', () => {
+  it('should return the GMT ISO string for a date', () => {
+    const date = new Date('2022-01-01T00:00:00Z');
+    const gmtISOString = getGmtISOString(date);
+
+    // As IST is 5:30 hours ahead of GMT, the date should be 18:30 hours of the previous day
+    expect(gmtISOString).toBe('2021-12-31T18:30:00.000Z');
+  });
+});
+
+describe('getNextDayGmtISOString - method to return next GMT date string from IST date', () => {
+  it('should return the GMT ISO string for the next day', () => {
+    const date = new Date('2022-01-01T00:00:00Z');
+    const nextDayGmtISOString = getNextDayGmtISOString(date);
+
+    // As IST is 5:30 hours ahead of GMT, the date should be 18:30 hours of the next day
+    expect(nextDayGmtISOString).toBe('2022-01-01T18:30:00.000Z');
+  });
+});
